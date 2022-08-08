2c62e2a2888e35e5a85e273c62b5ea578ee70440
Ale <ploerp@163.com>
2022-08-09 00:03:55 +0800

    新增通用工具类与响应数据类

---
+++ mmw-common/src/main/java/com/wanern/mmw/common/response/ResponseData.java
@@ -0,0 +1,8 @@
+package com.wanern.mmw.common.response;
+
+/**
+ * 通用响应类
+ * Created by Ale on 2022/8/9
+ */
+public class ResponseData {
+}
---
+++ mmw-common/src/main/java/com/wanern/mmw/common/utils/ESUtils.java
@@ -0,0 +1,8 @@
+package com.wanern.mmw.common.utils;
+
+/**
+ * 通用工具类
+ * Created by Ale on 2022/8/9
+ */
+public class ESUtils {
+}
