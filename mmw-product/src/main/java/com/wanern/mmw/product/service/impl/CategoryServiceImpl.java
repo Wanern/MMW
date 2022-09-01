23441be7ce82a8287e8e7df87e2176af1390413d
Ale <ploerp@163.com>
2022-09-01 17:41:02 +0800

    商品分类服务包优化

--- mmw-product/src/main/java/com/wanern/mmw/product/service/impl/CategoryServiceImpl.java
+++ mmw-product/src/main/java/com/wanern/mmw/product/service/impl/CategoryServiceImpl.java
@@ -2,7 +2,6 @@ package com.wanern.mmw.product.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
